2c219fd0a3bed7dcda42ed61b5aa7ff13f924ac3
Drake Nguyen <drakeknguyen@yahoo.com>
2020-11-08 22:27:53 -0800

    Version 1.0.14

--- server.js
+++ server.js
@@ -54,6 +54,13 @@ app.use(function (err, req, res, next) {
 if (process.env.NODE_ENV === "production") {
   //Set static folder
   app.use(express.static("client/build"));
+  app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'"],
+      imgSrc: ["'self"]
+    }
+  }))
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
